4addffc701e2d355b6f9ef06e3e81f6d9c8dff1b
FidrausJawed01 <firdausj.ug20.ee@nitp.ac.in>
2021-09-20 14:29:48 +0530

    hoop problem

---
+++ hoop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<iomanip>
+using namespace std;
+
+int main (){
+  int t;
+  cin>>t;
+  while (t--)
+  {  
+    int n;
+    cin>>n;
+    int last_no_hoop= (n+1)/2;
+    cout<<last_no_hoop<<endl;
+ }
+    return 0;
+}
