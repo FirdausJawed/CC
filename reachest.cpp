4b0e6b0bfa56b8c70b60863d51542c10957fb355
FidrausJawed01 <firdausj.ug20.ee@nitp.ac.in>
2021-09-21 20:51:46 +0530

    added another question

---
+++ reachest.cpp
@@ -0,0 +1,17 @@
+// https://www.codechef.com/LTIME97C/problems/CHFRICH
+
+#include <iostream>
+#include<iomanip>
+using namespace std;
+
+int main (){
+  int t;
+  cin>>t;
+  while (t--)
+  {  
+
+    
+    
+    }
+    return 0;
+} 
