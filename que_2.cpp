25ee2d0c2424916aa3d28ea51d5351586961e9b1
Firdaus Jawed <85278666+FirdausJawed@users.noreply.github.com>
2021-09-26 16:46:30 +0530

    another question

---
+++ que_2.cpp
@@ -0,0 +1,39 @@
+
+#include <iostream>
+#include<iomanip>
+using namespace std;
+
+int main (){
+ int t;
+  cin>>t;
+  while (t--)
+  {
+     long a,b;
+     cin>>a>>b;
+     
+     int n=1;
+     int x =0;
+     if(a>b){
+     while(a%b !=0){
+        n++;
+     }
+     }
+      if(a<b){
+     while(b%a!=0){
+        n++;
+     }
+     }
+      a = n*a;
+      x=x+a;
+      x=x/b;
+     if (x == 1)
+     {
+        cout<<"YES"<<endl;
+     }
+     else{
+          cout<<"NO"<<endl;
+     }
+     
+   }
+    return 0;
+}
