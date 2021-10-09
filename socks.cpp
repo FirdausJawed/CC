08e3eb40eb3cc934018827f1ab738ce6453b2bb5
Firdaus Jawed <85278666+FirdausJawed@users.noreply.github.com>
2021-10-09 13:42:38 +0530

    4

---
+++ socks.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int a,b,c;
+cin>>a>>b>>c;
+
+if (a==b==c || a==b || b==c || a==c)
+{
+    cout<<"YES"<<endl;
+}
+else{
+        cout<<"NO"<<endl;
+
+}
+
+	return 0;
+}
